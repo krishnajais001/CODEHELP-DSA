d1630de477e1c100f9c5a3d74d887035e1fbff20
Krishna Kumar <krishnajais7091@gmail.com>
2026-05-16 01:18:03 +0530

    Add initial C++ program files and user input functionality

--- CPP PROGRAMMING BASICS/main.cpp
+++ CPP PROGRAMMING BASICS/main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hello <<Krishna";
+    // cout<<"Hello <<Krishna";
+
+    // User Input in C++:
+    int age ;
+    cout<<"Enter Your Age : ";
+    cin >> age ;
+    cout<<"Your age is : "<< age <<endl;
+
 
     return 0;
 }
