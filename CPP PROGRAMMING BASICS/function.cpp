d934b208eb7f468d74cd1ec1fb4e345eaee111c6
Krishna Kumar <krishnajais7091@gmail.com>
2026-05-18 23:59:54 +0530

    Add comments to explain function concepts and variable scope

--- CPP PROGRAMMING BASICS/function.cpp
+++ CPP PROGRAMMING BASICS/function.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
-
+/*
+    Function -> 
+    Syntax ->
+    Variable Scope :
+        Local , Global Block and how to acces it
+    pass by value
+    call by refernce
+*/
 
 
     return 0;
